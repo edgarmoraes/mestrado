e7e4de74d81c2736596f513678083805a136520e
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2024-11-22 19:07:12 +0000

    Atualiza├º├úo autom├ítica pelo script .bat

--- abrir_jupyter.bat
+++ abrir_jupyter.bat
@@ -5,5 +5,17 @@ call C:\Users\edgar\anaconda3\Scripts\activate.bat
 rem Use o diretório onde o script .bat está localizado
 cd "%~dp0"
 
+rem Atualize o repositório do GitHub (pull)
+git pull
+
+rem Adicione todas as mudanças locais ao Git
+git add .
+
+rem Faça um commit com uma mensagem automática
+git commit -m "Atualização automática pelo script .bat"
+
+rem Envie as mudanças para o GitHub (push)
+git push origin main
+
 rem Abra o Jupyter Lab
 jupyter lab
