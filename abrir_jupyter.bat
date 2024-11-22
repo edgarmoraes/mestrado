ba3b25fad97f4e62e559c6e68aee524fe8c89d64
edgarmoraes <102760451+edgarmoraes@users.noreply.github.com>
2024-11-22 18:54:13 +0000

    second commit

---
+++ abrir_jupyter.bat
@@ -0,0 +1,9 @@
+@echo off
+rem Abra o terminal da Anaconda
+call C:\Users\edgar\anaconda3\Scripts\activate.bat
+
+rem Mude para o diretório desejado
+cd "C:\Users\edgar\OneDrive\Documents\2. Master's\Programação para Ciência de Dados"
+
+rem Abra o Jupyter Lab
+jupyter lab
